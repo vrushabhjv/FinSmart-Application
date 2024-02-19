265366098ccd07ebc1ed2137299761e594f64333
vrushabhjv <vrushabhjv2@gmail.com>
2024-02-19 23:13:29 +0530

    Created Dev environment for project

---
+++ FinSmart/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+    # path('logout/', views.logout, name='logout'),
+    # path('profile/', views.profile, name='profile'),
+    # path('edit_profile/', views.edit_profile, name='edit_profile'),
+    # path('change_password/', views.change_password, name='change_password'),
+    # path('delete_profile/', views.delete_profile, name='delete_profile'),
+]
