79ef533c9773d0ef9cce763e4db4cc443cc6c752
vrushabhjv <vrushabhjv2@gmail.com>
2024-02-22 02:05:02 +0530

    Added basic register

--- FinSmart/users/views.py
+++ FinSmart/users/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    pass
+    return render(request, 'home.html')
+
+def register(request):
+    # print("Login")
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        confirm_password=request.POST['confirm_password']
+        # confirm_password=password
+        
+        if password != confirm_password:
+            messages.error(request, 'Password does not match')
+            return redirect('register')
+        else:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.success(request, 'You have successfully registered')
+                return redirect('home')
+    else:
+        return render(request, 'register.html')
