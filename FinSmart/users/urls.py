b257f6fc8c62d4eb4783360a97cc51b2c1144001
vrushabhjv <vrushabhjv2@gmail.com>
2024-02-23 02:19:16 +0530

    Added login funtionality

--- FinSmart/users/urls.py
+++ FinSmart/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register', views.register, name='register'),
-    # path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     # path('logout/', views.logout, name='logout'),
     # path('profile/', views.profile, name='profile'),
     # path('edit_profile/', views.edit_profile, name='edit_profile'),
--- FinSmart/users/views.py
+++ FinSmart/users/views.py
@@ -34,3 +34,23 @@ def register(request):
                 return redirect('home')
     else:
         return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
