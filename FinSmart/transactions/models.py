9ce4a6f5294fcf843dfaa6a0167d29f0347722ae
vrushabhjv <vrushabhjv2@gmail.com>
2024-06-06 19:57:35 +0530

    minor changes

--- FinSmart/transactions/models.py
+++ FinSmart/transactions/models.py
@@ -11,6 +11,10 @@ class TransactionCategory(models.Model):
         verbose_name_plural = 'Transaction Categories'
     def __str__(self):
         return self.name
+    # The __str__method in Django models is used to provide a human-readable string representation of the model instance. It is a special method in Python that is called when you try to convert an object to a string, such as when printing the object or using it in a template.
+    # For example, if you have a `Transaction` model, the `__str__` method can be used to display the transaction details in a more user-friendly way, such as `"John Doe - Groceries - Bought milk"` instead of the default string representation, which might be something like `"<Transaction object (1)>"`.
+
+# By providing a meaningful string representation of the model instance, the `__str__` method can greatly improve the readability and usability of your Django application, especially when working with large datasets or complex models.
     
     
 class Transaction(models.Model):
