1c931e467cd2f94597b8cc60259bdad2b8292afb
vrushabhjv <vrushabhjv2@gmail.com>
2024-07-09 19:32:59 +0530

    Testing For deployment

--- FinSmart/FinSmart/settings.py
+++ FinSmart/FinSmart/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'transactions',
-    'goals',
+    # 'users.apps.UsersConfig',
+    # 'transactions',
+    # 'goals',
 ]
 
 MIDDLEWARE = [
